073ff2cc2a7eaea12c3071a9227889fa9aa466c3
Luis Henrique <carvalholuis839@gmail.com>
2025-06-30 18:46:44 -0300

    feat: atualizacoes em index e script

---
+++ src/script.js
@@ -0,0 +1,78 @@
+class Produto {
+    constructor(nome, preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
+
+class Carrinho {
+    constructor() {
+        this.itens = [];
+    }
+
+    adicionar(produto) {
+        const itemExistente = this.itens.find(p => p.produto.nome === produto.nome);
+        if (itemExistente) {
+            itemExistente.quantidade++;
+        } else {
+            this.itens.push({ produto, quantidade: 1 });
+        }
+        this.exibir();
+    }
+    calcularTotal() {
+        return this.itens.reduce((total, item) => {
+            return total + (item.produto.preco * item.quantidade);
+        }, 0);
+    }
+
+    exibir() {
+        const carrinhoDiv = document.getElementById('carrinho');
+        carrinhoDiv.innerHTML = '';
+
+        if (this.itens.length === 0) {
+            carrinhoDiv.innerHTML = '<p>Carrinho vazio.</p>';
+        } else {
+            this.itens.forEach(item => {
+                carrinhoDiv.innerHTML += `
+                    <p>
+                        ${item.produto.nome} - R$ ${item.produto.preco.toFixed(2)} x ${item.quantidade}
+                    </p>
+                `;
+            });
+        }
+
+        document.getElementById('total').innerText = this.calcularTotal().toFixed(2);
+    }
+}
+
+const produtosDisponiveis = [
+    new Produto("Camiseta", 49.90),
+    new Produto("Calça Jeans", 89.90),
+    new Produto("Jaqueta", 199.90),
+    new Produto("Vestido", 149.90),
+    new Produto("Boné", 29.90)
+];
+
+const carrinho = new Carrinho();
+
+function listarProdutos() {
+    const container = document.getElementById('produtos');
+    produtosDisponiveis.forEach(produto => {
+        const div = document.createElement('div');
+        div.className = 'produto';
+        div.innerHTML = `
+            <h3>${produto.nome}</h3>
+            <p>R$ ${produto.preco.toFixed(2)}</p>
+            <button>Adicionar ao Carrinho</button>
+        `;
+
+        const botoes = div.querySelectorAll('button');
+        botoes[0].addEventListener('click', () => {
+            carrinho.adicionar(produto);
+        });
+
+        container.appendChild(div);
+    });
+}
+
+listarProdutos();
